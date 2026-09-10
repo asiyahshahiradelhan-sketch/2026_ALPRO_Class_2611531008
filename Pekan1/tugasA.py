c1ad09927cfebc229d19e15d16b0cdfd5efa6eca
asiyahshahiradelhan-sketch <asiyahshahiradelhan@gmail.com>
2026-09-10 17:06:52 +0700

    Tugas Laporan Praktikum1

---
+++ Pekan1/tugasA.py
@@ -0,0 +1,9 @@
+nama = "Siti"
+nilai_matematika = 85
+nilai_bahasa = 90
+nilai_inggris = 80
+
+rata_rata = (nilai_matematika + nilai_bahasa + nilai_inggris) / 3
+
+print("Nama:", nama)
+print("Rata-rata:", rata_rata)
